4b22c391b54e45ca3596df011f55ecb3fa9b8734
Moise-is-sleeping <mbrebre1909@g.educaand.es>
2023-11-20 20:57:23 +0100

    navigation added

---
+++ app/src/main/java/com/calculator/blackjackmoise/model/Routes.kt
@@ -0,0 +1,9 @@
+package com.calculator.blackjackmoise.model
+
+sealed class Routes(val route : String) {
+    object Player1 : Routes("Player 1")
+    object MultiplayerScreen : Routes("MultiplayerScreen")
+    object MainMenu : Routes("MainMenu")
+
+
+}
